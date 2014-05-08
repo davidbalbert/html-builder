e994de62445623b598ec912dace8c7f40ac65b72
David Albert <davidbalbert@gmail.com>
2014-05-08 18:13:31 -0400

    Cleanup

--- html.rb
+++ html.rb
@@ -1,22 +1,42 @@
-class HTML
+class HTML < BasicObject
   class Tag
     attr_reader :name, :children, :attributes
 
     def initialize(name, attributes = {}, children = [])
-      @name = name
+      @name = name.to_s
       @attributes = attributes
       @children = children
     end
 
-    def children?
+    def has_children?
       children.size > 0
     end
 
+    def has_attributes?
+      attributes.size > 0
+    end
+
+    def should_always_have_closing_tag?
+      name == "script"
+    end
+
     def to_s
-      if children?
-        "<#{name}>#{children.map(&:to_s).join}</#{name}>"
+      if has_children?
+        "<#{name}#{attribute_string}>#{children.map(&:to_s).join}</#{name}>"
+      elsif should_always_have_closing_tag?
+        "<#{name}#{attribute_string}></#{name}>"
       else
-        "<#{name} />"
+        "<#{name}#{attribute_string} />"
+      end
+    end
+
+  private
+
+    def attribute_string
+      if has_attributes?
+        " #{attributes.map { |k, v| "#{k}=\"#{v}\"" }.join(" ")}"
+      else
+        ""
       end
     end
   end
@@ -39,16 +59,16 @@ class HTML
     attributes = args.first || {}
 
     if block
-      child_context = self.class.new
+      child_context = ::HTML.new
       res = child_context.instance_eval(&block)
 
-      if child_context.has_tags?
-        tag = Tag.new(name.to_s, attributes, child_context.tags)
+      unless res.is_a? Tag
+        tag = Tag.new(name, attributes, [res])
       else
-        tag = Tag.new(name.to_s, attributes, [res])
+        tag = Tag.new(name, attributes, child_context.tags)
       end
     else
-      tag = Tag.new(name.to_s, attributes)
+      tag = Tag.new(name, attributes)
     end
 
     @tags << tag
